6708ef825eb5b082756137d818b26143832510ac
Rhedd <devrhedd@gmail.com>
2023-08-17 14:11:09 -0300

    again homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,24 @@
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
 
 
-const Home = () => ('Dude, Where is my home?')
+const Home = () => {
+
+    return (
+        <div className='container home-page'>
+            <div className="text-zone">
+                <h1>Hi! <br /> I'm
+                <img src={LogoTitle} alt='developer' />
+                looban
+                <br />
+                Web Developer 
+                </h1>
+                <h2>Frontend developer / Javascrip expert</h2>
+                <Link to='/contact' className='flat-button'>Contact Me</Link>
+            </div>
+        </div>
+    );
+}
 
 export default Home;
